6b3d480f6103b5bc9f8f530a4c310ec556175773
Malonyz <mapmap1234@hotmail.com>
2020-02-28 17:02:01 +0700

    gg

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -25,3 +25,41 @@ int main(){
 }
 
 //Write definition of randData(), showData() and findRowSum()
+void randData(double  *dPtr,int N,int M){
+	for (int i=0;i<(N*M);i++){
+		*dPtr = double(rand()%100+1)/100;
+		dPtr++;	
+
+	}
+}
+
+void findRowSum(const double *dPtr,double *result,int N,int M){
+	double sum;
+	
+	for(int i=0;i<N;i++){
+		
+		sum =0;
+		for(int j=0;j<M;j++){
+			
+			 sum +=*dPtr;
+			 dPtr++;
+
+			
+		}
+		*(result+i) = sum;
+		
+		
+	}
+
+}
+void showData(double *dPtr,int N,int M){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<M;j++){
+			cout << *dPtr<< " ";
+			dPtr++;
+
+		}
+		cout <<"\n";
+	}
+
+}
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -25,3 +25,40 @@ int main(){
 }
 
 //Write definition of randData(), showData() and findColSum()
+void randData(double  *dPtr,int N,int M){
+	for (int i=0;i<(N*M);i++){
+		*dPtr = double(rand()%100+1)/100;
+		dPtr++;	
+
+	}
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+	double sum;
+	
+	for(int i=0;i<N;i++){
+		sum =0;
+		
+		
+		for(int j=0;j<M;j++){
+			
+			 sum += *(a+i+j*N);
+			
+		}
+		*(b+i) = sum;
+		
+		
+	}
+
+}
+void showData(double *dPtr,int N,int M){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<M;j++){
+			cout << *dPtr<< " ";
+			dPtr++;
+
+		}
+		cout <<"\n";
+	}
+
+}
